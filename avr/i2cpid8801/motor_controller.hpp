eeeda356daf7b615ac20e6cbdc2bd9e301208a83
Martin Llofriu <mllofriu@irobot.com>
2019-01-15 19:25:40 -0800

    remove unneded variables

--- avr/i2cpid8801/motor_controller.hpp
+++ avr/i2cpid8801/motor_controller.hpp
@@ -118,10 +118,6 @@ private:
     int16_t m_target_i{0};
     /// Maximum allowed e * ki
     int16_t m_max_e_ki{0};
-    /// The accumulated error times the prop constant
-    int16_t m_pe{0};
-    /// The accumulated error times the integral constant
-    int16_t m_ie{0};
     /// The state of the encoders
     bool m_quad_a, m_quad_b;
     /// The accumulated error times the prop constant
